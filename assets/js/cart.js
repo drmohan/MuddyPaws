79a7e6a1ff8fccddf3e87aa81a731d7a06182ca3
Divya <drmohan@andrew.cmu.edu>
2017-10-21 16:01:16 -0400

    implemented quantity add and delete functionality

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -1,6 +1,21 @@
+function getCartSize() {
+    var keys = Object.keys(localStorage);
+    var total = 0;
+    for (var i = 0; i < keys.length; i++){
+        var key = keys[i]
+        var item = JSON.parse(localStorage.getItem(key));
+        var quantity = item.quantity;
+        console.log(quantity)
+        total += quantity
+    }
+    return total
+}
+
 $(document).ready(function() {
 
-    // $('#cart_items').text(localStorage.length); 
+    var cartSize = getCartSize();
+    $('#cart_items').text(cartSize);
+
 
     var keys = Object.keys(localStorage);
     for (var i = 0; i < keys.length; i++){
@@ -12,19 +27,30 @@ $(document).ready(function() {
         var product = item.product;
         var size = item.size;
         var color = item.color;
+        var quantity = item.quantity;
 
-        var html = "<tr><td>" + id + "</td><td>" + product + "</td><td>" + size + "</td><td>" + color + "</td><td>" + "1" + "</td><td>" + "$10.80" + "</td><td class='remove'>x</td></tr>"
+        var html = "<tr><td>" + id + "</td><td>" + product + "</td><td>" + size + "</td><td>" + color + "</td><td>" + quantity + "</td><td>" + "$10.80" + "</td><td class='remove'>x</td></tr>"
  
         $("#my_table").append(html);
     }
 
     $('.remove').click( function() {
         var parent = $(event.target).parent();
-        console.log(parent[0].cells[0].innerHTML)
-        console.log(parent)
         var to_remove = parent[0].cells[0].innerHTML;
-        localStorage.removeItem(to_remove);
-        parent.remove()
+        var item = JSON.parse(localStorage.getItem(to_remove));
+
+        if (item.quantity == '1') {
+            localStorage.removeItem(to_remove);
+            parent.remove()
+        }
+        else {
+            item.quantity -= 1;
+            localStorage.setItem(to_remove, JSON.stringify(item))
+            parent[0].cells[4].innerHTML = JSON.parse(localStorage.getItem(to_remove)).quantity
+        }
+        
+        cartSize = getCartSize();
+        $('#cart_items').text(cartSize);
     });
 
 });
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,70 @@
-// var key = localStorage.length + 1;
+
+function getCartSize() {
+    var keys = Object.keys(localStorage);
+    var total = 0;
+    for (var i = 0; i < keys.length; i++){
+        var key = keys[i]
+        var item = JSON.parse(localStorage.getItem(key));
+        var quantity = item.quantity;
+        console.log(quantity)
+        total += quantity
+    }
+    return total
+}
+
+function productInCart(product, size, color) {
+    
+    var keys = Object.keys(localStorage);
+    for (var i = 0; i < keys.length; i++){
+
+        var key = keys[i];
+        var item = JSON.parse(localStorage.getItem(key));
+
+        if (product == item.product && size == item.size && color == item.color) {
+            return item.id
+        } 
+    }
+    return null
+}
 
 $(document).ready(function() {
+
+    var cartSize = getCartSize();
+    $('#cart_items').text(getCartSize);  
 
     $("#myForm").submit( "click", function(e) {
     	e.preventDefault();
 
-        var id = Math.floor(Math.random() * 100);
-        console.log(id)
     	var product = $('#product').val();
     	var size = $('#size').val();
     	var color = $('#color').val();
+        var quantity;
+
+        var id = productInCart(product, size, color);
+        if (id) {
+            quantity = JSON.parse(localStorage.getItem(id)).quantity + 1;
+        } else {
+            id = Math.floor(Math.random() * 100);
+            while(! id in localStorage) {
+                id = Math.floor(Math.random() * 100);
+            }
+            quantity = 1;
+        }
+
 
         var item = {
             'id': id,
             'product': product,
             'size': size,
-            'color':color
+            'color': color,
+            'quantity': quantity
         };
 
         var jsonItem = JSON.stringify(item);
-
     	localStorage.setItem(id, jsonItem);
-        // key += 1;
 
-        $('#cart_items').text(localStorage.length);  
-
-
+        var cartSize = getCartSize();
+        $('#cart_items').text(getCartSize);  
 	});
 
 });
